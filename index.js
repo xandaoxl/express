296588efdac0a0c00c64e974fa149c37e217f3cc
xandaoxl <barrosxloficial@gmail.com>
2024-08-05 09:47:48 -0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// add//
+const express = require ('express')
+const app = express()
+const port = 3001
+
+app.get('/', (req, res) => {
+  res.send('OLA EXPRESS')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
