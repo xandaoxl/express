31641537f52fb2a4873d4ea071f1c90c7ea2109e
xandaoxl <barrosxloficial@gmail.com>
2024-08-05 11:41:09 -0300

    add 3000

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require ('express')
 const app = express()
-const port = 3001
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('OLA EXPRESS')
+  res.send('olá mundo')
 })
 
 app.listen(port, () => {
